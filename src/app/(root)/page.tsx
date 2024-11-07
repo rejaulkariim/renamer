2e2d577621a18a6c46976ae14e4df3cade893b63
karimscoding <rejaulkarim66666@gmail.com>
2024-11-07 13:09:24 +0600

    feat: add seo metadata

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Showcase all your startups on a single page",
+  description:
+    "All your apps, revenue, and links in one place. The Linktree for entrepreneurs. Create your Indie Page for free!",
+};
+
+export default function Home() {
+  return <div>hello</div>;
+}
