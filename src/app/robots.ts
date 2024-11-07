e8b606d9db3ff52ee9568caac859f38165206ab6
karimscoding <rejaulkarim66666@gmail.com>
2024-11-07 13:31:58 +0600

    ci: modified robots.txt to allow all crawlers access

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -4,13 +4,8 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: [
       {
-        userAgent: "Googlebot",
+        userAgent: "*",
         allow: ["/"],
-        disallow: "/private/",
-      },
-      {
-        userAgent: ["Applebot", "Bingbot"],
-        disallow: ["/"],
       },
     ],
     sitemap: "https://renamer.me/sitemap.xml",
