89f9c32afee2eea203bfb907da816c2a2ec21574
karimscoding <rejaulkarim66666@gmail.com>
2024-11-07 12:46:27 +0600

    ci: config changes

---
+++ src/config/site.ts
@@ -0,0 +1,13 @@
+import { SiteConfig } from "@/types";
+
+export const siteConfig: SiteConfig = {
+  name: "Renamer",
+  description:
+    "Your personal indie page to showcase all your saas startups and everything you create.",
+  url: "https://renamer.me",
+  ogImage: "https://renamer.me/og.png",
+  links: {
+    twitter: "https://twitter.com/keepeer",
+    github: "https://github.com/rejaulkariim/keepeer",
+  },
+};
